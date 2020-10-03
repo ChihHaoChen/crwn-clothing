2c8a0acdbd285a7f9718814eb8d7c9c48881ca0a
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-10-03 18:54:45 +0800

    Add checkout page, checkout item component and update cart to use selectors.

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+import { createSelector, createStructuredSelector } from 'reselect';
 import CartItem from '../../components/cart-item/cart-item.component';
 
 const selectCart = state => state.cart;
@@ -8,9 +8,22 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+)
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems], // always choose smaller sets of data, rather than from state.cart
   cartItems => cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + CartItem.quantity, 0
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
   )
 );
+
+export const selectCartTotal = createSelector(
+  [selectCartItems], // always choose smaller sets of data, rather than from state.cart
+  cartItems => cartItems.reduce(
+    (accumulatedQuantity, cartItem) => 
+      accumulatedQuantity + cartItem.quantity * cartItem.price, 0
+  )
+)
